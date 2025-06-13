4eab11598ed409927776ae70cc00f0c2d4cc6b33
sameer302 <sameernilkhan11@gmail.com>
2025-06-13 19:35:38 +0530

    learn about variables

---
+++ secondprogram.py
@@ -0,0 +1,3 @@
+print("My name is Sameer", "My bro is idiot")
+print(728943)
+print(20*30)
---
+++ variables.py
@@ -0,0 +1,9 @@
+# a variable is a name given to a memory location in a program
+# variable_name = value
+name = "sameer"
+age = 23
+college = "IIT Bombay"
+print("My college is:",college)
+age2 = age
+print(age2)
+# 33:12
